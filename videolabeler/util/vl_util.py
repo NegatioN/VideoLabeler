e27bb17638b10d4d6e2e01089235e83002c8f1c1
NegatioN <joakimrishaug@gmail.com>
2016-12-11 20:29:32 +0100

    add option to save images as pickled dataframe

--- videolabeler/util/vl_util.py
+++ videolabeler/util/vl_util.py
@@ -3,12 +3,14 @@
 import imageio
 import os
 import logging
+import pandas as pd
 
 
 class Util:
 
     def __init__(self):
         self.logger = logging.getLogger('video_labeler')
+        self.labeled_df = pd.DataFrame(columns=['image_data', 'label'])
 
     def frames(self, start, end=0, metadata=None):
         fps = metadata['fps']
@@ -19,13 +21,15 @@ class Util:
         self.logger.debug('Time start: {} end:{}, Frames start: {} end: {}'.format(start, end, start_frame, end_frame))
         return start_frame, end_frame
 
-
     def save_image(self, folder, index, image, filename):
         if os.path.isdir(folder) is False:
             os.makedirs(folder)
         imageio.imwrite('{}/{}-{}.jpg'.format(folder, filename, index), image)
         self.logger.info('Saved image={}-{} in {}'.format(filename, index, folder))
 
+    def add_image_data(self, np_image_array, label):
+        image_df = pd.DataFrame(np_image_array.reshape(-1, 3))  # reshape from 3dimensional array to 2d
+        self.labeled_df.append(image_df, label)
 
-    def save_pickle(self, image_df, name):
-        pass
+    def save_pickle(self, path, label):
+        self.labeled_df.to_pickle('{}/{}.pickle'.format(path, label))
--- videolabeler/videolabeler/video_labeler.py
+++ videolabeler/videolabeler/video_labeler.py
@@ -18,7 +18,8 @@ parser.add_argument("-p", "--path", required=True, help="Path to video-file")
 parser.add_argument("-m", "--mode", default='folder', choices=modes, help="Save-mode")
 parser.add_argument("-st", "--step", default=5, help="Number of frames to step forward each iteration.")
 parser.add_argument("-v", "--verbose", type=bool, default=False, help="Output debug-info?")
-#parser.add_argument("-f", "--format", default="ffmpeg", help="Format to decode video with")
+parser.add_argument("-f", "--format", default="ffmpeg",
+                    help="Format to decode video with. List of options at http://imageio.readthedocs.io/en/latest/formats.html")
 
 options = parser.parse_args()
 
@@ -35,22 +36,28 @@ else:
 
 util = vl_util.Util()
 
-def main():
-    #TODO support taking in arrays of labels and timings?
 
+def main():
+    # TODO support taking in arrays of labels and timings?
+    # TODO option to save as grayscale images. require significantly less disk-space / memory to hold.z
     filepath = options.path
     filename = path_leaf(filepath)[:-4]
-    savefolder = 'output' if options.mode is modes[1] else 'output/{}'.format(options.label)
+    savefolder = 'output' if options.mode == modes[1] else 'output/{}'.format(options.label)
 
-    with imageio.get_reader(filepath,  'ffmpeg') as video_reader:
+    with imageio.get_reader(filepath, format=options.format) as video_reader:
         start_frame, end_frame = util.frames(options.start, options.end, video_reader.get_meta_data())
         frame_num = start_frame
         step = options.step
         while frame_num <= end_frame:
             video_frame = video_reader.get_data(frame_num)
-            util.save_image(savefolder, frame_num, video_frame, filename)
+            if options.mode == modes[1]:
+                util.add_image_data(video_frame, options.label)
+            elif options.mode == modes[0]:
+                util.save_image(savefolder, frame_num, video_frame, filename)
             frame_num += step
-    logger.info('Labeled {} frames as {}'.format((end_frame-start_frame/step), options.label))
+    if options.mode == modes[1]:
+        util.save_pickle(savefolder, options.label)
+    logger.info('Labeled {} frames as {}'.format(((end_frame - start_frame) / step), options.label))
 
 
 def path_leaf(path):
